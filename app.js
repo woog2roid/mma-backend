ecbc4cb6657840f92919e5960c9d0303aee3a14c
woog2roid <wooguijung@korea.ac.kr>
2022-01-26 09:42:27 +0000

    app.js만 있는 서버 제작

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const { api } = require('mma-api');
+require('dotenv').config({ path: '../.env'});
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(cors({
+	origin: ['https://woog2roid.github.io'],
+}));
+
+app.use((req, res, next) => {
+	if(process.env.NODE_ENV == 'production') {
+		morgan('combined')(req, res, next);
+	} else {
+		morgan('dev')(req, res, next);
+	}
+});
+
+app.get('/', (req, res, next) => {
+	api(req.query.fighter, (data) => {
+		try {
+			if (!req.query.fighter) {
+				//URL이 잘못됨: fighter query가 없는 오류
+				res.status(400).send({code:'query error'});
+				return;
+			} else {
+				res.status(200).json(data);
+				return;
+			}
+		} catch (err) {
+			next(err);
+		}
+	});
+});
+
+app.use((err, req, res, next) => {
+	console.log('[서버 내부 오류 발생], 오류 내용 이하.');
+	console.log(err);
+	return res.sendStatus(500);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
